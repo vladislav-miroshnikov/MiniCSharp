3d588ba083bec214370eb2f3c5dd346f1aa267fa
Vladislav <vladislaw.miroshnikov@gmail.com>
2020-11-18 15:35:30 +0300

    new ast added

--- CsharpExcep/lib/ast.ml
+++ CsharpExcep/lib/ast.ml
@@ -1,102 +1,77 @@
-module AST = struct
-  type csType = CsInt | CsBool | CsVoid | CsChar | CsString | CsRef of string
+type data_type = CsInt | CsVoid | CsString | CsClass of string
+[@@deriving show]
 
-  type csValue =
-    | CsVInt of int
-    | CsVBool of bool
-    | CsVVoid
-    | CsVNull
-    | CsVChar of char
-    | CsVString
-    | CsVRef
+type value =
+  | CsVInt of int
+  | CsVBool of bool
+  | CsVVoid
+  | CsVNull
+  | CsVChar of char
+  | CsVString of string
+  | CsVClass
+[@@deriving show]
 
-  type csModifier = Public | Static | Override | Const
+type modifier = Public | Static | Override | Const [@@deriving show]
 
-  type csVariable =
-    { modifiers: csModifier list
-    ; varType: csType
-    ; varName: string
-    ; value: csValue option }
+type expr =
+  | Add of expr * expr
+  | Sub of expr * expr
+  | Mul of expr * expr
+  | Div of expr * expr
+  | Mod of expr * expr
+  | PostInc of expr (*x++*)
+  | PostDec of expr (*x--*)
+  | PrefInc of expr (*++x*)
+  | PrefDec of expr (*--x*)
+  | And of expr * expr
+  | Or of expr * expr
+  | Not of expr
+  | Equal of expr * expr
+  | NotEqual of expr * expr
+  | Less of expr * expr
+  | More of expr * expr
+  | LessOrEqual of expr * expr
+  | MoreOrEqual of expr * expr
+  | Null
+  | Access of expr * expr
+  | Const of value
+  | IdentObj of string
+  | ClassCreate of string * expr list (*first - name, other - args list*)
+  | CallMethod of expr * expr list
+  | Assign of expr * expr
+[@@deriving show]
 
-  type csException = {excep: csType; excepMessage: string}
+and statement =
+  | For of statement option * expr option * expr list * statement
+  | If of expr * statement * statement option (*statement option is "else"*)
+  | While of expr * statement (*option?*)
+  | Break
+  | Continue
+  | Return of expr option
+  | VarDeclare of data_type * (expr * expr option) list
+  | Expression of expr
+  | Throw of expr
+  | StatementBlock of statement list
+  | Try of
+      statement
+      * ((data_type * expr option) option * expr option * statement) list
+      * statement option (*WARN!*)
+[@@deriving show]
 
-  type csExpr =
-    | DeclareExpr
-    | NumExpr
-    | LogicExpr
-    | CompareExpr
-    | CallMethod of {name: string; args: csExpr list}
-    | Null
-    | Const of csValue
-    | Variable of string
-    | Access of csExpr * csExpr
-    | Many of csExpr * csExpr
+and field =
+  | VariableField of modifier list * data_type * (expr * expr option) list (*example: static int a = 3, b*)
+  | Method of
+      modifier list
+      * data_type
+      * expr
+      * (data_type * expr) list
+      * statement option
+  | Constructor of modifier list * expr * (data_type * expr) list * statement
+[@@deriving show]
 
-  and csNumExpr =
-    | Add of csExpr * csExpr
-    | Sub of csExpr * csExpr
-    | Mul of csExpr * csExpr
-    | Div of csExpr * csExpr
-    | Mod of csExpr * csExpr
-    | PostInc of csExpr
-    | PostDec of csExpr
-    | PrefInc of csExpr
-    | PrefDec of csExpr
+and csClass =
+  | Class of
+      modifier list * expr (*name*) * expr option (*parent class*) * field list
+[@@deriving show]
 
-  and csDeclareExpr =
-    | ClassDeclare of {className: string; classArgs: csExpr list}
-
-  and csLogicExpr =
-    | And of csExpr * csExpr
-    | Or of csExpr * csExpr
-    | Not of csExpr
-
-  and csCompareExpr =
-    | Equal of csExpr * csExpr
-    | NotEqual of csExpr * csExpr
-    | Less of csExpr * csExpr
-    | More of csExpr * csExpr
-    | LessOrEqual of csExpr * csExpr
-    | MoreOrEqual of csExpr * csExpr
-
-  and csStatement =
-    | Expr of csExpr
-    | Break
-    | Continues
-    | If of
-        { condition: csExpr
-        ; thenStatement: csStatement list
-        ; elseStatement: csStatement list }
-    | While of {condition: csExpr; body: csStatement list}
-    | For of
-        { varDec: csStatement
-        ; condition: csExpr
-        ; afterBody: csStatement list
-        ; body: csStatement list }
-    | Return of {result: csExpr}
-    | VarDec of {vType: csType; vName: string; vValue: csValue option}
-    | Throw of {exc: csException}
-    | Try of
-        { catches: csStatement list
-        ; filters: csStatement list
-        ; finally: csStatement option }
-    | Catch of {excepName: csException option}
-    | Filter of {excepName: csException; condition: csExpr}
-    | Finally
-
-  type csMethod =
-    { mModifiers: csModifier list
-    ; mType: csType
-    ; mName: string
-    ; mArgs: csExpr list
-    ; mBody: csStatement list }
-
-  type csClass =
-    { cModifiers: csModifier list
-    ; cName: string
-    ; cFields: csVariable list
-    ; cMethods: csMethod list
-    ; cClasses: csClass list }
-
-  type csPrint = PrintF of {exp: csExpr}
-end
+type csPrint = PrintF of expr [@@deriving show]
