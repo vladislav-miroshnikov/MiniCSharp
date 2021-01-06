72a6e6638152465d2ec976b431e0c607986c883f
Lyubov <miloslubov@gmail.com>
2021-01-06 18:48:32 +0300

    - minor fix

--- Scheme/src/main/scala/example/Main.scala
+++ Scheme/src/main/scala/example/Main.scala
@@ -6,7 +6,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     // for example (+ 1 2)
-    println("Print smth, for example, (+ 1 2)")
+    println("Enter smth (for example, (+ 1 2)) :")
     val input: String = readLine()
     val ans = Parser.apply(input)
     println(ans)
