6bbdbc85391a0829bfb5106a351f3a6b153da8c6
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2021-04-02 22:40:36 +0300

    Deleting unnecessary files

--- Lambda/REPL.ml
+++
@@ -1,4 +0,0 @@
-open Lambda_lib.Lambda
-
-let x = Var "a"
-let () = print_endline "REPL not implemented"
--- Lambda/lib/tests.ml
+++
@@ -1,4 +0,0 @@
-(* ***** AUTOMATIC TESTS HERE *)
-let rec fact n = if n = 1 then 1 else n * fact (n - 1)
-
-let%test _ = fact 5 = 120
